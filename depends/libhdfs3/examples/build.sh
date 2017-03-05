267d80311908428d5df827021ff6e7159596e1df
Sapan Shah <soup@immuta.com>
2017-03-05 14:59:32 -0500

    Add OSX special case

--- depends/libhdfs3/examples/build.sh
+++ depends/libhdfs3/examples/build.sh
@@ -4,4 +4,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DIST_DIR="${SCRIPT_DIR}/../dist"
 
 g++ -o hdfstest -I ${DIST_DIR}/include -L ${DIST_DIR}/lib -l hdfs3 filesystem.cpp
-install_name_tool -change libhdfs3.1.dylib $DIST_DIR/lib/libhdfs3.1.dylib hdfstest
+OS=`uname -s`
+if [ $OS == 'Darwin' ]; then
+    install_name_tool -change libhdfs3.1.dylib $DIST_DIR/lib/libhdfs3.1.dylib hdfstest
+fi
