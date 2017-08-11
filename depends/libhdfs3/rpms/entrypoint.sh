9b492487ab2d663d0f5694e4c11d35e684b7890a
Sapan Shah <soup@immuta.com>
2017-08-11 17:39:04 -0400

    Support both Redhat builds

--- depends/libhdfs3/rpms/entrypoint.sh
+++ depends/libhdfs3/rpms/entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-source /opt/rh/devtoolset-3/enable
+RHEL_VERSION=$(cat /etc/redhat-release | awk {'print $3'} | awk -F'.' '{print $1}')
+echo $RHEL_VERSION
+if [[ $RHEL_VERSION -eq 6 ]]
+then
+    source /opt/rh/devtoolset-3/enable
+fi
 pushd /libhdfs/libhdfs3/rpms
 ./build.sh run
