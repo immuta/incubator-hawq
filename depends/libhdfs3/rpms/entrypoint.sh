b2387ff186d220f41b26b563e4c486220080af51
Jeff Hastings <jeffmhastings@gmail.com>
2018-08-15 14:00:42 -0400

    devtoolset 7

--- depends/libhdfs3/rpms/entrypoint.sh
+++ depends/libhdfs3/rpms/entrypoint.sh
@@ -4,7 +4,7 @@ RHEL_VERSION=$(cat /etc/redhat-release | awk {'print $3'} | awk -F'.' '{print $1
 echo $RHEL_VERSION
 if [[ $RHEL_VERSION -eq 6 ]]
 then
-    source /opt/rh/devtoolset-6/enable
+    source /opt/rh/devtoolset-7/enable
 fi
 pushd /libhdfs/libhdfs3/rpms
 ./build.sh run
